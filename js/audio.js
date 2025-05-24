d2b665c09d5dcdabdc1f3697f39c2c08eb4873a1
Kacper Kowalski <xperski@pm.me>
2025-05-24 04:15:40 +0200

    auto-commit

--- js/audio.js
+++ js/audio.js
@@ -189,7 +189,7 @@ async function initPlayer() {
 
     overlayScreen.addEventListener('click', async () => {
         overlayScreen.style.display = 'none';
-        await playSong(12);
+        await playSong(songId);
         background.style.display = 'flex';
         content.style.display = 'flex';
         playPause.innerHTML = pauseIcon;
